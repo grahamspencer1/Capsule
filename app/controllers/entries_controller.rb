fb49afb9e82c89187963299d260e983c2b47038d
Valerie Osei <valerie.osei@gmail.com>
2018-09-17 16:53:41 -0400

    adding an index method to our entries controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,17 +1,40 @@
 class EntriesController < ApplicationController
 
   def index
+    @entries = Entries.all
   end
 
   def show
-
+    @entry = Entries.find(params[:id])
   end
 
   def create
+    @entry = Entries.new
+    @entry.title = params[:entry][:title]
+    @entry.content = params[:entry][:content]
+
+    # if params[:private] == "0"
+    #   @title.private = false
+    # end
+    #
+    # @entry.bg_picture_id
+  end
+
+  def new
 
   end
 
+  def destroy
 
+  end
+
+  def update
+
+  end
+
+  def edit
+
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :entries 
+  resources :entries
 
 #this makes the root of the whole app the entry's index page, showing all the entries
   root "entries#index"
-
-
 
 end
