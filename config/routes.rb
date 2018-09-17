424869134820b8287012af193a605b53813af131
Valerie Osei <valerie.osei@gmail.com>
2018-09-17 16:25:40 -0400

    defined our root route, and tested with a header in our entry, index view pg

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
+#this makes the root of the whole app the entry's index page, showing all the entries
+  root "entries#index"
 
 
 end
