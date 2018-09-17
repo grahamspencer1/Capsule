d46292a2e4b7fd7a84170941ec807e0a9d90f2cf
Valerie Osei <valerie.osei@gmail.com>
2018-09-17 16:29:43 -0400

    created 7 standard routes for entries

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :entries 
 
 #this makes the root of the whole app the entry's index page, showing all the entries
   root "entries#index"
+
 
 
 end
