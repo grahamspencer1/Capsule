2f75d402445db41c4912bd863003f1ce036e7abc
Graham Spencer <grahamspence@gmail.com>
2018-09-18 10:48:25 -0400

    new user page online

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,25 @@
 class UsersController < ApplicationController
-  
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+    @user.username = params[:user][:username]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to entries_url
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
   resources :entries
-  
 
 #this makes the root of the whole app the entry's index page, showing all the entries
   root "entries#index"
+
+  get 'login' => 'user_sessions#new', :as => :login
+  delete 'logout' => 'user_sessions#destroy', :as => :logout
+  resources :users, only: [:new, :create, :show]
+  resources :user_sessions, only: [:create]
 
 end
