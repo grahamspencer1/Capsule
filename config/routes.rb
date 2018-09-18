7ea5cc125c896b0a1fc1d6fce7d7571970016e1b
ilyana19 <aries_2152@hotmail.com>
2018-09-18 17:08:53 -0400

    fixed login/signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :entries
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
-  
+  get 'users' => 'users#new'
+  get 'sessions' => 'sessions#new'
+
   root 'sessions#new'
 end
