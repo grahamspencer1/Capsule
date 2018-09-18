1402b44aee788a5a1daf720b97ae2f0a164d2df7
Graham Spencer <grahamspence@gmail.com>
2018-09-18 11:02:35 -0400

    new user button works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user.username = params[:user][:username]
+    @user.name = params[:user][:name]
     @user.email = params[:user][:email]
     @user.password = params[:user][:password]
     @user.password_confirmation = params[:user][:password_confirmation]
