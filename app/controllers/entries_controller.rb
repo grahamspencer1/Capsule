e2da645fc9f08a168fece0ce369754276c8cd773
Graham Spencer <grahamspence@gmail.com>
2018-09-19 11:46:37 -0400

    got entry submission working

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -18,19 +18,22 @@ class EntriesController < ApplicationController
     @entry = Entry.new
     @entry.title = params[:entry][:title]
     @entry.content = params[:entry][:content]
+    @entry.user = current_user
 
     if params[:private] == false
       @title.private = false
     end
 
     @entry.bg_picture_id = 1
+    @entry.mood = "Neutral"
+    @entry.auto_mood = false
 
     if @entry.save
       flash[:alert] = "Success!"
       redirect_to "/entries"
     else
       flash[:alert] = "Failure!"
-      render :new
+      render "entries/new"
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_19_145401) do
+ActiveRecord::Schema.define(version: 2018_09_19_153719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bg_pictures", force: :cascade do |t|
-    t.integer "entry_id"
     t.string "mood"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
