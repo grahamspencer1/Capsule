d4d6dc5b7d15471b3b38bfcdbd270d51fd230c2a
Graham Spencer <grahamspence@gmail.com>
2018-09-19 17:47:12 -0400

    added delete and edit functions to entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -49,6 +49,25 @@ class EntriesController < ApplicationController
 
   def update
     @entry = Entry.find(params[:id])
+
+    @entry.title = params[:entry][:title]
+    @entry.content = params[:entry][:content]
+    @entry.user = current_user
+
+    if params[:private] == false
+      @title.private = false
+    end
+
+    @entry.bg_picture_id = 1
+    @entry.mood = "Neutral"
+    @entry.auto_mood = false
+    
+    if @entry.save
+      flash[:alert] = "Successfully updated entry"
+      redirect_to "/entries/#{@entry.id}"
+    else
+      render :edit
+    end
   end
 
   def edit
