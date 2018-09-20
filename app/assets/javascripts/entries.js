b15a47b052c1da0c25b6ff318c569458c43a8d24
Graham Spencer <grahamspence@gmail.com>
2018-09-20 17:17:25 -0400

    changed up confirmation message

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded' , function(){
     checkbox.addEventListener('change', function (e) {
       if (!(e.target.checked)) {
         var txt;
-        var r = confirm("WARNING - Unchecking this box will make this entry viewable in the Anonymous Entries section. Your personal information won't be posted, but be aware that the complete entry will be viewable to the public. Are you absolutely sure you want to share this anonymously to the public?");
+        var r = confirm("WARNING - Unchecking this box will make your entry visible to other Capsule users. Your screen name, email, and other personal information will not be shown, but the content itself will. Are you sure you want to share this with the world?");
         if (r == true) {
           console.log("Okay");
         } else {
