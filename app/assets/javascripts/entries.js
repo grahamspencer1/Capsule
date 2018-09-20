eb30126382da7f55630556344b102990c4d3a161
Graham Spencer <grahamspence@gmail.com>
2018-09-20 16:59:24 -0400

    added warning for when a post is set to public

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -1,16 +1,14 @@
-  var entryIndex = 1;
-  showEntry(entryIndex);
-
-  function goEntry(n) {
-  showEntry(entryIndex += n);
-  }
-  function showEntry(n) {
-      var i;
-      var x = document.selectqueery("currentEntry");
-      if (n > x.length) {entryIndex = 1}
-      if (n < 1) {entryIndex = x.length} ;
-      for (i = 0; i < x.length; i++) {
-          x[i].style.display = "none";
+document.addEventListener('DOMContentLoaded' , function(){
+  var checkbox = document.querySelector('#entry_private')
+    checkbox.addEventListener('change', function (e) {
+      if (!(e.target.checked)) {
+        var txt;
+        var r = confirm("WARNING - Unchecking this box will make this entry viewable in the Anonymous Entries section. Your personal information won't be posted, but be aware that the complete entry will be viewable to the public. Are you absolutely sure you want to share this anonymously to the public?");
+        if (r == true) {
+          console.log("Okay");
+        } else {
+          e.target.checked = true;
+        }
       }
-      x[entryIndex-1].style.display = "block";
-  }
+    });
+});
