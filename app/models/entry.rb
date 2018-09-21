9721597b7148ca053cd93bd9490b16e0bdd217af
ilyana19 <aries_2152@hotmail.com>
2018-09-21 12:20:46 -0400

    fixed entry search showing ALL entries, even ones not belonging to current user

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,14 +4,14 @@ class Entry < ApplicationRecord
 
   validates :title, length: { minimum: 3, maximum: 50 }, presence: true
   validates :content, length: { minimum: 3, maximum: 1500 }, presence: true
-  
+
   def self.last_five_entry(current_user)
     result = current_user.entries.order('id desc').limit(5)
     return result
   end
 
-  def self.entry_search(date)
-    Entry.where("created_at >= ? ", date)
+  def self.entry_search(current_user, date)
+    current_user.entries.where("created_at >= ? ", date)
   end
 
 end
