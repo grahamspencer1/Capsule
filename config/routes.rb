e0385b11b436af244ade2708e5346454b1396838
Graham Spencer <grahamspence@gmail.com>
2018-09-23 13:42:03 -0400

    added random button to public entries

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   get 'entries/public' => 'entries/public'
+  get 'entries/random' => 'entries#random'
+
   resources :entries
   resources :bg_pictures
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+  
   get 'users' => 'users#new'
   get 'sessions' => 'sessions#new'
-
   root 'sessions#new'
-
 end
