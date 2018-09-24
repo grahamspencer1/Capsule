81e675c590a06415f2bb363fe63db48bc93e17c0
ilyana19 <aries_2152@hotmail.com>
2018-09-24 14:13:27 -0400

    fixed the entry update with auto mood on

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   get 'entries/public' => 'entries/public'
-  get 'entries/random' => 'entries#random'
+  # get 'entries/random' => 'entries#random'
 
   resources :entries
   resources :bg_pictures
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
-  
+
   get 'users' => 'users#new'
   get 'sessions' => 'sessions#new'
   root 'sessions#new'
