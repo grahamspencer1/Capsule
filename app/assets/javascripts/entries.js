ad6a60e347fbf0b13313aff1b996bb1c3a4a1db6
ilyana19 <aries_2152@hotmail.com>
2018-09-24 17:48:01 -0400

    removed obsolete commented code

--- app/assets/javascripts/entries.js
+++ app/assets/javascripts/entries.js
@@ -28,26 +28,8 @@ document.addEventListener('DOMContentLoaded' , function(){
   var footer = document.querySelector("footer");
 
   if (nav && nav.classList.contains("visible")) {
-    // nav.classList.remove("hidden");
     footer.classList.add("nav-mod");
   } else {
     footer.classList.remove("nav-mod");
   }
-
-  //dynamically change entry show page background
-  // var entries = document.querySelectorAll(".entry-container");
-  // var bg = document.querySelector(".bg2");
-  //
-  // entries.forEach(function(entry) {
-  //   entry.addEventListener("click", function(e) {
-  //     // e.preventDefault();
-  //
-  //     $.ajax({
-  //       url: "/entries",
-  //       method: "GET"
-  //     }).done(function(responseData) {
-  //       bg.style.backgroundImage = "url('https://images.unsplash.com/photo-1536573538316-9b0e89ad26d3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=23c93f420502b90ce60d751314022d3a&auto=format&fit=crop&w=1950&q=80')";
-  //     });
-  //   });
-  // });
 });
