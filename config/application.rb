af37864b65de05921731af91b6ca53c29746d34c
ilyana19 <aries_2152@hotmail.com>
2018-10-22 13:16:42 -0400

    randomized seeds, also changed sort entries by entry date

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Capsule
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Eastern Time (US & Canada)'
     config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
